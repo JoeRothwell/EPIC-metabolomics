fd9c488b23df9de8d21ccae83d4c5000ddb1ebc3
JoeRothwell <joerothwell1@gmail.com>
2018-11-13 01:23:49 +0100

    Improved match function

--- Feature_matching.R
+++ Feature_matching.R
@@ -2,68 +2,77 @@
 
 # Get common features between CS and HCC datasets and use this as a starting point for analysis
 
-feature.match <- function(RTtol = 0.1, study = c("cs", "hcc")) {
+feature.match <- function(RTtol = 0.1, study = c("cs", "hcc"), mode = c("pos", "neg")) {
 
   library(haven)
   library(tidyverse)
   library(fuzzyjoin)
-  # read in CS data and HCC data
+  # Get CS data and HCC data (feature names only)
+  # Pos or neg mode for CS and HCC
   
-  # CS: 
-  cspos <- read_tsv("data/EPIC Cross sectional RP POS Feature table.txt", skip=4) %>% select(1) %>% mutate(CS.feat = 1:n())
-  csneg <- read_tsv("data/EPIC Cross sectional RP NEG Feature table.txt", skip=4) %>% select(1) %>% mutate(CS.feat = 1:n())
-  
-  # HCC
-  hccpos <- read_csv("data/EPIC liver cancer 2016 RP POS feature table.csv") %>% 
+  if(mode == "pos") {
+  cs <- read_tsv("data/EPIC Cross sectional RP POS Feature table.txt", skip=4) %>% select(1) %>% mutate(CS.feat = 1:n())
+  hcc <- read_csv("data/EPIC liver cancer 2016 RP POS feature table.csv") %>% 
     select(1) %>% slice(-(83:84)) %>% mutate(HCC.feat = 1:n())
-  hccneg <- read_csv("data/EPIC liver cancer 2016 RP Neg Feature Table.csv") %>% 
+  
+    } else if (mode == "neg") {
+      
+  cs <- read_tsv("data/EPIC Cross sectional RP NEG Feature table.txt", skip=4) %>% select(1) %>% mutate(CS.feat = 1:n())
+  hcc <- read_csv("data/EPIC liver cancer 2016 RP Neg Feature Table.csv") %>% 
     select(1) %>% mutate(HCC.feat = 1:n())
+  }
   
-  # Join pos mode features
-  csfeat <- cspos  %>% separate(Compound, into = c("Mass", "RT"), sep = "@", convert = T)
-  hccfeat <- hccpos %>% separate(Compound, into = c("Mass", "rt"), sep = "@", convert = T)
+  # Join features
+  csfeat  <- cs %>% separate(Compound, into = c("Mass", "RT"), sep = "@", convert = T)
+  hccfeat <- hcc %>% separate(Compound, into = c("Mass", "rt"), sep = "@", convert = T)
   
-  posjoin <- difference_inner_join(csfeat, hccfeat, max_dist = 0.005, distance_col = "massdiff") %>% 
-    filter(abs(RT - rt) < 0.1) #%>% arrange(CS.feat)
-  
-  # Join neg mode features
-  
-  csfeat1 <- csneg  %>% separate(Compound, into = c("Mass", "RT"), sep = "@", convert = T)
-  hccfeat1 <- hccneg %>% separate(Compound, into = c("Mass", "rt"), sep = "@", convert = T)
-  
-  negjoin <- difference_inner_join(csfeat1, hccfeat1, max_dist = 0.005, distance_col = "massdiff") %>% 
+  joindf <- difference_inner_join(csfeat, hccfeat, max_dist = 0.005, distance_col = "massdiff") %>% 
     filter(abs(RT - rt) < RTtol) #%>% arrange(CS.feat)
+  
+  return(joindf)
   
   # 2922 features matched in pos and 1243 in neg mode
   
   # Get unique vector of CSS features that matched. These are then used in Intake_correlation to filter starting FTs
-  
-  if(study == "cs") {
-  
-    v1 <- unique(posjoin$CS.feat)
-    v2 <- unique(negjoin$CS.feat)
-  
-    } else if (study == "hcc") {
-    
-    v1 <- unique(posjoin$HCC.feat)
-    v2 <- unique(negjoin$HCC.feat)
-  
-    }
-  output <- list(v1, v2)
-}  
+  if(study == "cs") v1 <- unique(joindf$CS.feat) else if (study == "hcc") v1 <- unique(joindf$HCC.feat)
+} 
 
+pos.match <- feature.match(mode = "pos")
+neg.match <- feature.match(mode = "neg")
 
-output1 <- feature.match(study = "cs")
-output2 <- feature.match(study = "hcc")
+# Vectors of matched features
+CSmatchpos <- unique(pos.match$CS.feat)
+CSmatchneg <- unique(neg.match$CS.feat)
+HCCmatchpos <- unique(pos.match$CS.feat)
+HCCmatchneg <- unique(neg.match$CS.feat)
 
 # ----
 
-CSmatchpos <- output1[[1]]
-CSmatchneg <- output1[[2]]
+# Use vectors to subset peak tables and run partial correlation function
+# Run functions from Intake_correlation script. Output is assigned alcpos.cs etc
 
-HCCmatchpos <- sort(output2[[1]])
-HCCmatchneg <- sort(output2[[2]])
+# Pos mode
 
+alcpos.cs1 <- alcpos.cs %>% select(Pcor : CS.feat)
+alcpos.hcc1 <- alcpos.hcc %>% select(Pcor : HCC.feat)
+pos.match1 <- pos.match %>% left_join(alcpos.cs1, by = "CS.feat") %>% 
+  left_join(alcpos.hcc1, by = "HCC.feat")
+
+# Plot correlations
+plot(pos.match1$Pcor.x, pos.match1$Pcor.y, xlab = "Correlation CS", ylab = "Correlation HCC")
+
+# Neg mode
+
+alcneg.cs1  <- alcneg.cs %>% select(Pcor : CS.feat)
+alcneg.hcc1 <- alcneg.hcc %>% select(Pcor : HCC.feat)
+neg.match1  <- neg.match %>% left_join(alcneg.cs1, by = "CS.feat") %>% 
+  left_join(alcneg.hcc1, by = "HCC.feat")
+
+# Plot correlations
+plot(neg.match1$Pcor.x, neg.match1$Pcor.y, xlab = "Correlation CS", ylab = "Correlation HCC")
+
+
+# ----
 
 # loop to get tolerance vs feature numbers (doesn't work yet)
 tol <- seq(0.05, 0.15, by  = 0.005)
