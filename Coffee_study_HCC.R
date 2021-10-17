4cd3ff826e2cdd16832560ff35e832f0ba953783
Joe <joerothwell1@gmail.com>
2021-10-18 00:45:34 +0200

    Formatted categorical analysis

--- Coffee_study_HCC.R
+++ Coffee_study_HCC.R
@@ -26,11 +26,13 @@ dat <- rbind(dat1, dat2)
 
 # Get compound names and subset intensities
 cmpds <- paste(dat$`Compound Name`, "_int", sep = "")
-ints <- t(dat[, -c(1:4)]) #%>% rownames_to_column()
+#ints <- t(dat[, -c(1:4)]) #%>% rownames_to_column()
+ints <- dat %>% select(contains("LivCan_")) %>% t
 Id_Bma <- rownames(ints)
 
 rownames(ints) <- NULL
 colnames(ints) <- cmpds
+df1 <- bind_cols(Id_Bma, ints)
 df <- cbind(Id_Bma, ints) %>% as_tibble() %>% 
   mutate(Id_Bma = str_sub(Id_Bma, start = -14L, end = -5L))
 
@@ -40,9 +42,9 @@ df1 <- inner_join(meta, df, by = "Id_Bma")
 
 # Subset intensities, scale, plot
 mat <- df1 %>% select(ends_with("_int"))
-#mat <- df1[, -(1:3)]
 mat <- data.matrix(mat, rownames.force = NA) %>% scale
 
+# Plot distributions
 plot.ts(mat[, 1:10], type = "p")
 plot.ts(mat[, 11:20], type = "p")
 
@@ -65,10 +67,21 @@ fits <- apply(mat, 2, multiclr)
 results <- map_df(fits, ~tidy(., exponentiate = T, conf.int = T)) %>% filter(term == "x") %>%
   bind_cols(cmpd = cmpds)
   
-RR.ci(data = results)
+#RR.ci(data = results)
+# Format OR and CI
+tabcat <- results %>%
+  mutate_at(vars(estimate:conf.high), ~ format(round(., digits = 2), nsmall = 2)) %>% 
+  mutate(B1 = " (", hyph = "-", B2 = ")") %>%
+  unite("OR", estimate, B1, conf.low, hyph, conf.high, B2, sep = "")
 
 # Categorical analysis
 catmat <- apply(mat, 2, function(x) cut_number(x, n = 4, labels = 1:4))
 fits1 <- apply(catmat, 2, multiclr)
-results1 <- map_df(fits1, ~tidy(., exponentiate = T)) %>% 
-  bind_cols(cmpd = rep(cmpds, each = 3))
+results1 <- map_df(fits1, ~tidy(., exponentiate = T, conf.int = T)) %>% 
+  filter(str_detect(term, "x")) %>% mutate(cmpd = rep(cmpds, each = 3))
+
+# Format OR and CI
+tabcat1 <- results1 %>%
+  mutate_at(vars(estimate:conf.high), ~ format(round(., digits = 2), nsmall = 2)) %>% 
+  mutate(B1 = " (", hyph = "-", B2 = ")") %>%
+  unite("OR", estimate, B1, conf.low, hyph, conf.high, B2, sep = "")
