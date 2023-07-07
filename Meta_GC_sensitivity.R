6dd04313b91021d072ac2fff5858a6454331d99b
JoeRothwell <joerothwell1@gmail.com>
2023-07-07 17:48:51 +0200

    Added model

--- Meta_GC_sensitivity.R
+++ Meta_GC_sensitivity.R
@@ -22,21 +22,35 @@ library(broom)
 res.adj <- map_df(pos.adj, tidy, exponentiate = T, conf.int = T) %>% 
   filter(term == "x") %>% mutate(p.adj = p.adjust(p.value, method = "fdr"), feature = features) %>%
   select(feature, everything(), -term) %>% mutate(feat.no = 1:length(pos.adj))
-
+# No significant features after p-value adjustment
 
 
 # (b)	run sensitivity analyses on the n=105 + n=71 (i.e., n=176) case-control pairs where the case is Hppos =1, 
 # but the control could be 1 or 0.
 
 # This is concordant11 + discordant10
-discordant10 <- discordant10 %>% select( -disc.type)
+discordant10 <- discordant10 %>% mutate(across((varlist), as.factor)) %>% select( -disc.type)
 caseposall <- bind_rows(concordant11, discordant10)
+posints <- caseposall %>% select(starts_with("Untg_Rp_Pos")) %>% as.matrix() %>% log()
+
+clr.adj <- function(x) clogit(Cncr_Caco_Stom.x ~ x + Bmi_C + Smoke_Stat + Pa_Total + 
+                                QE_ENERGY + QE_ALC + L_School + Fasting_C + 
+                                strata(Match_Caseset.x), data = caseposall)
+
+# Apply model across subset
+pos.adj <- apply(posints, 2, clr.adj)
+
+res.adj <- map_df(pos.adj, tidy, exponentiate = T, conf.int = T) %>% 
+  filter(term == "x") %>% mutate(p.adj = p.adjust(p.value, method = "fdr"), feature = features) %>%
+  select(feature, everything(), -term) %>% mutate(feat.no = 1:length(pos.adj))
+# No significant features after p-value adjustment
 
 
 # (c)	Possibly consider an analysis on the n=11 + n=17 where the cases are Hppos negative, but this will be weak.
 
 
 # (d)	Break matching and run unconditional analyses by Hppos, but with adjustment for country instead of by centre.
+
 
 
 # (e)	In all sub-group analyses a, b, c and d above, also run analyses on the subset of n=230 who have missing data, 
